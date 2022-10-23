cf31cab0be8f55e0eafbd2f9bac843c43c7a11ef
masa <scs_lo_ol_728@icloud.com>
2022-10-23 12:06:11 +0900

    ✨ lint-staged 動作確認&導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'plugin:react/recommended',
-		'standard-with-typescript',
-		'plugin:react-hooks/recommended',
-		'prettier'
-	],
-	overrides: [],
-	ignorePatterns: ['.eslintrc.*'],
-	parserOptions: {
-		project: ['./tsconfig.json'],
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['react'],
-	rules: {
-		'react/jsx-uses-react': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'@typescript-eslint/consistent-type-definitions': 'off',
-	},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  ignorePatterns: ['.eslintrc.*'],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+  },
 };

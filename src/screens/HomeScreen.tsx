9c1198bd5b902fe08af5a36d8a315fca45f8b036
masa <scs_lo_ol_728@icloud.com>
2022-10-31 22:21:46 +0900

    ✨ NativeBase使用

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Avatar } from 'native-base';
 import React from 'react';
 import { Text, View } from 'react-native';
 import { useTailwind as tw } from 'tailwind-rn/dist';
@@ -21,6 +22,14 @@ export const HomeScreen = ({ navigation }: Props): JSX.Element => {
   return (
     <View style={tw()('flex-1 justify-center')}>
       <Text style={{ color: colors.text }}>Home</Text>
+      <Avatar
+        bg="indigo.500"
+        source={{
+          uri: 'https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        }}
+      >
+        JB
+      </Avatar>
     </View>
   );
 };
